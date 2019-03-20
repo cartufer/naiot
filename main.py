e4b3be2506f020876dd10d8c520c2c05cbb2e517
cartufer <31514298+cartufer@users.noreply.github.com>
2019-03-20 12:32:22 -0500

    slimDNS and threading
    slimDNS might still not yet be working, testing required, threading is fine but race condition problems may exist

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import wifimgr
+import _thread
+import time
+def testThread():
+  while True:
+    import network
+    import slimDNS
+    sta_if = network.WLAN(network.STA_IF)
+    local_addr = sta_if.ifconfig()[0]
+    server = slimDNS.SlimDNSServer(local_addr, "micropython")
+    print("starting slimDNS server")
+    server.run_forever()
 
 
 wlan = wifimgr.get_connection()
@@ -9,4 +20,13 @@ if wlan is None:
 
 
 # Main Code goes here, wlan is a working network.WLAN(STA_IF) instance.
-print("ESP OK")
+print("ESP OK, wifi connected")
+#import gc
+#gc.collect()
+#gc.enable()
+#def testThread():#server.run_forever()
+
+_thread.start_new_thread(testThread, ())
+#for i in range(1):
+_thread.start_new_thread(testThread, ())
+print("mdns setup complete")
